2c3d315e953d74874a11e640514d02145d2cef28
tamourax <at074402@gmail.com>
2026-08-06 14:26:52 +0300

    remove all code in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,10 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Row(
-          spacing: 20,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.yellow,
-              child: Center(
-                child: Text(
-                  'Hello, World!',
-                  style: TextStyle(fontSize: 32, color: Colors.blue),
-                ),
-              ),
-            ),
-
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.yellow,
-              child: Center(
-                child: Text(
-                  'Hello, World!',
-                  style: TextStyle(fontSize: 32, color: Colors.blue),
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.yellow,
-              child: Center(
-                child: Text(
-                  'Hello, World!',
-                  style: TextStyle(fontSize: 32, color: Colors.blue),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ),
+   MyApp(),
   );
 }
+
+
